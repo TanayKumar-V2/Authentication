8211ec3e4dcfa9dfbb16d3d625dabcdbb2f503a3
TanayKumar-V2 <tanayk726@gmail.com>
2025-06-07 17:41:18 +0530

    Final

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -30,7 +30,8 @@ const handler= NextAuth({
 
 
   })
-]
+],
+secret: process.env.NEXTAUTH_SECRET,
 })
 
 export{handler as GET, handler as POST}
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,9 @@
-"use client";
 
+import { getServerSession } from "next-auth";
 import { useSession, SessionProvider, signOut, signIn } from "next-auth/react";
 
 export default function Home(){
-  return(
-    <SessionProvider>
-      <RealHome />
-    </SessionProvider>
-  )
-}
+  const session= await getServerSession()
 
-function RealHome(){
-  const session=useSession()
-
-  return(
-    <div className="grid grid-rows-[20px-1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      {session.status==="authenticated" && <button onClick={()=>signOut()}>Logout</button>}
-      {session.status==="unauthenticated" && <button onClick={()=>signIn()}>Login</button>}
-    </div>
-  )
+  
 }
