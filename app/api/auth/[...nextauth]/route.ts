83e2faa52b5e10e50fcf77912afcadd3d4036b60
TanayKumar-V2 <tanayk726@gmail.com>
2025-06-07 17:14:17 +0530

    Init

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,36 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler= NextAuth({
+  providers: [
+  CredentialsProvider({
+   
+    name: "Login with E-mail",
+    
+    credentials: {
+      username: { label: "Username", type: "text", placeholder: "example@email.com" },
+      password: { label: "Password", type: "password" }
+    },
+    async authorize(credentials, req) {
+      const username=credentials?.username
+      const password=credentials?.password
+
+      const user={
+        name:"Tanay",
+        id:"1",
+        username:"tanay@email.com"
+      }
+
+      if(user){
+        return user
+      }else{
+        return null
+      }
+    }
+
+
+  })
+]
+})
+
+export{handler as GET, handler as POST}
